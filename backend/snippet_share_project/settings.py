ab6a93faece51486b1f40b8e5cbc4aca8f467e90
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-07-18 18:06:41 +0600

    - added cors url

--- backend/snippet_share_project/settings.py
+++ backend/snippet_share_project/settings.py
@@ -128,6 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://jahangir1x.github.io',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
